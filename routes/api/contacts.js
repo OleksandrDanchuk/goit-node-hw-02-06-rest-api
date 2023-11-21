be33b5b3a2d2d5769ef659b89408c0c81a52a0f3
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-11-21 21:35:00 +0200

    doin

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,31 @@
 const express = require('express');
-
 const router = express.Router();
+const ctrl = require('../../controllers/contacts');
+const validateBody = require('../../middlewars/validateBody');
+const { addSchema, updateFavoriteSchema } = require('../../schemas/contacts');
+const isValidId = require('../../middlewars/isValidId');
+const validateFavoriteBody = require('../../middlewars/validateFavoriteBody');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: '123' });
-});
+router.get('/', ctrl.listContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: '123' });
-});
+router.get('/:contactId', isValidId, ctrl.getContactById);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+router.post('/', validateBody(addSchema), ctrl.addContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+router.delete('/:contactId', isValidId, ctrl.removeContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+router.put(
+  '/:contactId',
+  isValidId,
+  validateBody(addSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  validateFavoriteBody(updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
